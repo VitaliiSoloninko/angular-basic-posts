ec2b35b56b29d4a12b31b7b93b2feff35b579111
22051988Di <vitalii.soloninko@gmail.com>
2025-04-12 17:55:53 +0200

    0.3 IPost model + @Input

--- src/app/posts/post-item/post-item.component.ts
+++ src/app/posts/post-item/post-item.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPost } from '../../../models/IPost';
 
 @Component({
   selector: 'app-post-item',
   imports: [],
   templateUrl: './post-item.component.html',
-  styleUrl: './post-item.component.css'
+  styleUrl: './post-item.component.css',
 })
 export class PostItemComponent {
-
+  @Input() post: IPost = {} as IPost;
 }
