9f5c1b57dbbb7d4f1a0f85456cd5b3d33d9cebe8
22051988Di <vitalii.soloninko@gmail.com>
2025-04-12 18:04:01 +0200

    0.4 ngFor posts or @for

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IPost } from '../../models/IPost';
 import { PostItemComponent } from './post-item/post-item.component';
 
 @Component({
   selector: 'app-posts',
-  imports: [PostItemComponent],
+  imports: [PostItemComponent, CommonModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css',
 })
-export class PostsComponent {}
+export class PostsComponent {
+  posts: IPost[] = [
+    { id: 1, title: 'JS 1', body: 'Body 1' },
+    { id: 2, title: 'JS 2', body: 'Body 2' },
+    { id: 3, title: 'JS 3', body: 'Body 3' },
+  ];
+}
